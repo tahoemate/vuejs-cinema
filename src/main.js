16c8a8499523f14364ffcca97e2eb06f937088c3
Brian Hale <bhale@dentrek.com>
2017-07-04 21:28:20 -0500

    fitlered components

--- src/main.js
+++ src/main.js
@@ -5,26 +5,50 @@ import genres from './util/genres';
 new Vue({
     el: '#app',
     data: {
-        msg: 'Hello Suckas'
+        msg: 'Hello Suckas',
+        genre: [],
+        time: []
     },
     methods: {
-        checkFilter() {
-            console.log( 'app - checkFilter' );
+        checkFilter(category, title, checked) {
+            console.log( 'app - checkFilter ' + title );
+            if (checked) {
+                this[category].push(title);
+            } 
+            else {
+                let index = this[category].indexOf(title);  // returns -1 if not found
+                if (index >= 0) {
+                    this[category].splice(index,1);  // remove item at index
+                }
+            }
         }
     },
     components: {
         'movie-list' : { 
             template: `<div id="movie-list">
-                <div v-for="movie in movies" class="movie">{{movie.title}}</div>
+                <div v-for="movie in filteredMovies" class="movie">{{movie.title}}</div>
                 </div>`,
             data() {
                 return { 
                     movies: [
-                        {title: 'Destry Rides Again'},
-                        {title: 'Blue Angle'},
-                        {title: 'Seven Sinners'}
+                        {title: 'Destry Rides Again', genre: genres.ANIMATION },
+                        {title: 'Blue Angle', genre: genres.COMEDY},
+                        {title: 'Seven Sinners', genre: genres.COMEDY}
                     ]
                 };
+            },
+            props: ['genre', 'time'],
+            methods: {
+                movieGenreFilter(movie) {
+                    if ( !this.genre.length ) return true;
+                    let aout = this.genre.find( genre => movie.genre === genre );
+                    return aout;
+                }
+            },
+            computed: {
+                filteredMovies() {
+                    return this.movies.filter(this.movieGenreFilter);
+                }
             }
         },
         'movie-filter' : { 
@@ -38,9 +62,9 @@ new Vue({
                             </div>
                         </div>`,
             methods: {
-                checkFilter () {
+                checkFilter (category, title, checked) {
                     console.log('movie-filter check-filter');
-                    this.$emit( 'check-filter' );
+                    this.$emit( 'check-filter', category, title, checked );
                 }
             },
             components: {
@@ -57,7 +81,7 @@ new Vue({
                         checkFilter () {
                             console.log('check-filter check-filter');
                             this.checked = !this.checked;
-                            this.$emit('check-filter');
+                            this.$emit('check-filter', 'genre', this.title, this.checked);
                         }
                     }
                 }
