006e2e1b14658a67d4af8a866c7646ec14e40cd3
Brian Hale <bhale@dentrek.com>
2017-07-05 11:46:50 -0500

    Convert to single file components

---
+++ src/components/MovieList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="movie-list">
+        <div v-for="movie in filteredMovies" class="movie">{{movie.title}}</div>
+    </div>
+</template>
+
+<script>
+    import genres from '../util/genres';
+
+    export default { 
+        data() {
+            return { 
+                movies: [
+                    {title: 'Destry Rides Again', genre: genres.ANIMATION },
+                    {title: 'Blue Angle', genre: genres.COMEDY},
+                    {title: 'Seven Sinners', genre: genres.COMEDY}
+                ]
+            };
+        },
+        props: ['genre', 'time'],
+        methods: {
+            movieGenreFilter(movie) {
+                if ( !this.genre.length ) return true;
+                let aout = this.genre.find( genre => movie.genre === genre );
+                return aout;
+            }
+        },
+        computed: {
+            filteredMovies() {
+                return this.movies.filter(this.movieGenreFilter);
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import './style.scss';
-import genres from './util/genres';
+
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
 
 new Vue({
     el: '#app',
@@ -11,7 +13,7 @@ new Vue({
     },
     methods: {
         checkFilter(category, title, checked) {
-            console.log( 'app - checkFilter ' + title );
+            // console.log( 'app - checkFilter ' + title );
             if (checked) {
                 this[category].push(title);
             } 
@@ -24,68 +26,7 @@ new Vue({
         }
     },
     components: {
-        'movie-list' : { 
-            template: `<div id="movie-list">
-                <div v-for="movie in filteredMovies" class="movie">{{movie.title}}</div>
-                </div>`,
-            data() {
-                return { 
-                    movies: [
-                        {title: 'Destry Rides Again', genre: genres.ANIMATION },
-                        {title: 'Blue Angle', genre: genres.COMEDY},
-                        {title: 'Seven Sinners', genre: genres.COMEDY}
-                    ]
-                };
-            },
-            props: ['genre', 'time'],
-            methods: {
-                movieGenreFilter(movie) {
-                    if ( !this.genre.length ) return true;
-                    let aout = this.genre.find( genre => movie.genre === genre );
-                    return aout;
-                }
-            },
-            computed: {
-                filteredMovies() {
-                    return this.movies.filter(this.movieGenreFilter);
-                }
-            }
-        },
-        'movie-filter' : { 
-            data() {
-                return { genres };
-            },
-            template: `<div id="movie-filter">
-                            <h2>Filter Results</h2>
-                            <div class="filter-group">
-                                <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
-                            </div>
-                        </div>`,
-            methods: {
-                checkFilter (category, title, checked) {
-                    console.log('movie-filter check-filter');
-                    this.$emit( 'check-filter', category, title, checked );
-                }
-            },
-            components: {
-                'check-filter': {
-                    data () {
-                        return { checked: false };
-                    },
-                    props: ['title'],
-                    template: `<div v-bind:class="{ 'check-filter': true, active: checked }" v-on:click="checkFilter">
-                                    <span class="checkbox"></span>
-                                    <span class="check-filter-title">{{title}}</span>
-                                </div>`,
-                    methods: {
-                        checkFilter () {
-                            console.log('check-filter check-filter');
-                            this.checked = !this.checked;
-                            this.$emit('check-filter', 'genre', this.title, this.checked);
-                        }
-                    }
-                }
-            }                                             
-        }
+        MovieList,  // auto converts to movie-list
+        MovieFilter  // auto converts to movie-filter
     }
 });
